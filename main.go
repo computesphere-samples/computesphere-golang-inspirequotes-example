fc7f6bd9f4fc95110be99f96f573b85fe79a25e6
Gagandeep Singh <gagansday@gmail.com>
2024-05-26 05:57:10 +0530

    update quotes

--- main.go
+++ main.go
@@ -8,11 +8,16 @@ import (
 )
 
 var quotes = []string{
-	"The best way to predict the future is to invent it.",
-	"Life is 10% what happens to us and 90% how we react to it.",
-	"The only way to do great work is to love what you do.",
-	"You miss 100% of the shots you don’t take.",
-	"The only limit to our realization of tomorrow is our doubts of today.",
+	"Code is like humor. When you have to explain it, it’s bad. – Cory House",
+	"First, solve the problem. Then, write the code. – John Johnson",
+	"Experience is the name everyone gives to their mistakes. – Oscar Wilde",
+	"In order to be irreplaceable, one must always be different. – Coco Chanel",
+	"Java is to JavaScript what car is to Carpet. – Chris Heilmann",
+	"Knowledge is power. – Francis Bacon",
+	"Sometimes it pays to stay in bed on Monday, rather than spending the rest of the week debugging Monday’s code. – Dan Salomon",
+	"Code never lies, comments sometimes do. – Ron Jeffries",
+	"Perfection is achieved not when there is nothing more to add, but rather when there is nothing more to take away. – Antoine de Saint-Exupéry",
+	"It’s not a bug – it’s an undocumented feature. – Anonymous",
 }
 
 func getRandomQuote() string {
